d5fe7f8862346f69fdf69b03ee2adf4cddb883b3
Dany Fot <lu4imira@yandex.ru>
2023-05-31 09:32:50 +0300

    finished inspirations section

---
+++ libs/swiper.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    centeredSlides: true,
+    spaceBetween:120,
+    slidesPerView: "auto",
+  
+
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },  
+
+  });
+  
